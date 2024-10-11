e1eab90aa2be6031ecaec36cf38024f663693b7b
Arsen Merkin <a.merkin@iva.ru>
2024-10-12 03:11:28 +0500

    update hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,19 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+const { PRIVATE_KEY, ALCHEMY_API_KEY } = process.env;
+
 const config: HardhatUserConfig = {
   solidity: "0.8.27",
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [PRIVATE_KEY as string],
+    },
+  },
+  etherscan: {
+    apiKey: "ВАШ_ETHERSCAN_API_KEY"
+  }
 };
 
 export default config;
